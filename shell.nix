65d5d106b7df480cd38465cf74b4bddb43c487d6
Rohan Hart <rohan.hart@fronde.com>
2020-02-10 13:14:17 +1300

    install tools via Nix and lorri

---
+++ shell.nix
@@ -0,0 +1,17 @@
+let
+  pkgs = import (builtins.fetchGit {
+    url = git://github.com/NixOS/nixpkgs-channels;
+    ref = "nixos-unstable";
+    rev = "d5e9b7d5a7c011605d7215b88efd70ccf8643ede";
+  }) {};
+in
+  pkgs.mkShell {
+    buildInputs = [
+      pkgs.nodejs-slim
+      pkgs.yarn
+      pkgs.jq
+    ];
+    shellHook = ''
+        export PATH="./node_modules/.bin/:$PATH"
+    '';
+  }
